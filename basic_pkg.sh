6572af763666485a883a7db5f526e76838709b2f
root <root@DESKTOP-PNQTL94.localdomain>
2020-04-09 11:15:02 -0400

    Updated

--- basic_pkg.sh
+++ basic_pkg.sh
@@ -1,5 +1,7 @@
 sudo apt update -y
-sudo apt install -y python python-pip python3-pip mc python-tk python3-tk x11-xserver-utils x11-apps openssh-server
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install -y python python-pip python3-pip mc python-tk python3-tk x11-xserver-utils x11-apps openssh-server traceroute
 pip install termcolor
 export DISPLAY="localhost:0.0"
 . ~/.bashrc
---
+++ ms_installer.sh
@@ -0,0 +1,3 @@
+apt update
+apt upgrade # Feel free to comment this
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
